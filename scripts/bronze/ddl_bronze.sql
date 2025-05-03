a3b49e7d5345a8f95bceb8be45234badbd172a89
Ayush Sachan <ayushsachan162@gmail.com>
2025-05-03 23:49:45 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,6 +9,7 @@ PURPOSE: This script Creates tables in Bronze Layer
 
 
 
+
 IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
    DROP TABLE bronze.crm_cust_info
 CREATE TABLE bronze.crm_cust_info (
@@ -21,6 +22,8 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_create_date DATE
 );
 
+IF OBJECT_ID ('bronze.crm_prd_info','U') IS NOT NULL
+   DROP TABLE bronze.crm_prd_info
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
@@ -31,6 +34,9 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_end_dt DATETIME
 );
 
+
+IF OBJECT_ID ('bronze.crm_sales_details','U') IS NOT NULL
+   DROP TABLE bronze.crm_sales_details
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
@@ -44,11 +50,16 @@ CREATE TABLE bronze.crm_sales_details (
 	
 );
 
+IF OBJECT_ID ('bronze.erp_loc_a101','U') IS NOT NULL
+   DROP TABLE bronze.erp_loc_a101
 CREATE TABLE bronze.erp_loc_a101(
    cid NVARCHAR(50),
    cntry NVARCHAR(50),
 );
 
+
+IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL
+   DROP TABLE bronze.erp_cust_az12
 CREATE TABLE bronze.erp_cust_az12(
    cid NVARCHAR(50),
    bdate DATE,
@@ -56,11 +67,15 @@ CREATE TABLE bronze.erp_cust_az12(
 );
 
 
-IF OBJECT_ID ('bronze.erp_px_cat_giv2','U') IS NOT NULL
-   DROP TABLE bronze.erp_px_cat_giv2
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+   DROP TABLE bronze.erp_px_cat_g1v2
 CREATE TABLE bronze.erp_px_cat_g1v2(
    id NVARCHAR(50),
    cat NVARCHAR(50),
    subcat NVARCHAR(50),
    maintanance NVARCHAR(50)
 );
+
+
+
+
